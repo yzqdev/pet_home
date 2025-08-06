3720490e30919cc4de008a6d44c81773e06a7f60
yzqdev <yzqdev@outlook.com>
2025-08-06 13:27:30 +0800

    remove unnecessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,3 @@ plugins {
     id("net.neoforged.moddev") version "2.0.80" apply (false)
 }
 
-allprojects {
-
-    layout.buildDirectory.set(File(rootDir, "build/${path.replace(":", "/")}"))
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,6 @@
 pluginManagement {
   repositories {
-    maven {
-      name = "Fabric"
-      url = uri("https://maven.fabricmc.net/")
-    }
-//    maven("https://maven.minecraftforge.net")
     maven("https://maven.neoforged.net/releases")
-
     mavenCentral()
     gradlePluginPortal()
   }
